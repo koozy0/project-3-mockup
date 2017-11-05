4bd0996c9b2207b1fc6c8f8fd6fe0eec3d2d6703
koozy <koozhengyu@gmail.com>
2017-11-06 01:01:53 +0800

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+  // login will show leftPane and show logut
+  // logout will hide leftPane and show login
+  var $loginLogout = $('#loginLogout')
+  $loginLogout.on('click', function() {
+    if($loginLogout.text() === 'Login') {
+      $loginLogout.text('Logout')
+      $('#leftPane').addClass('reveal');
+      // $('#restaurant').addClass('show-resto');
+    }
+    else {
+      $loginLogout.text('Login')
+      $('#leftPane').removeClass('reveal');
+      $('#restaurant').removeClass('show-resto');
+    }
+  })
+
+  // hide resto pin first
+  $('#restoPin').hide();
+  // show restaurant pane and pin when clicking on groups
+  var $restaurant = $('#restaurant');
+  var $groupItems = $('.list-group-item');
+  $groupItems.on('click', function() {
+    if($restaurant.hasClass('show-resto')) {
+      $restaurant.removeClass('show-resto');
+      $('#restoPin').hide();
+    }
+    else {
+      $restaurant.addClass('show-resto');
+      $('#restoPin').show();
+    }
+  })
+
+  // show and hide navbar
+  $('#navbarSideButton').on('click', function() {
+    $('#navbarSide').addClass('reveal');
+    $('.overlay').show();
+  });
+
+  $('.overlay').on('click', function(){
+    $('#navbarSide').removeClass('reveal');
+    $('.overlay').hide();
+  });
+});
